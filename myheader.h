69495a4fa3597cf2153e0e96088532ba86436808
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-11-26 20:00:24 +0200

    Create myheader.h

---
+++ myheader.h
@@ -0,0 +1,13 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+// Function to print a single character
+void ft_putchar(char c);
+
+// Function to print an integer
+void ft_putnbr(int n);
+
+// Function to print a formatted string
+int ft_printf(const char *format, ...);
+
+#endif
