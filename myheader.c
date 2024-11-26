e581d43397b9b8cbc654b4d21e5ab0c546b6b604
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-11-26 20:01:04 +0200

    Create myheader.c
    functions that help me in the ft_printf.c file

---
+++ myheader.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void ft_putchar(char c) {
+    write(1, &c, 1);   
+}
+
+void ft_putnbr(int n) {
+    if (n == -2147483648)
+    {
+        ft_putchar('-');
+        ft_putchar('2');
+        n = 147483648;
+    }
+    if (n<0)
+    {
+        ft_putchar('-');
+        n = -n;
+    }
+    if (n>=10)
+    {
+        ft_putnbr(n /10);
+    }
+    ft_putchar(n%10 + '0');
+    
+}
