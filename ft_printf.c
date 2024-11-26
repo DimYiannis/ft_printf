bfe9e07849db33e566d71084d109fb0e9dc6b5a7
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-11-26 20:04:09 +0200

    Update ft_printf.c
    - variadic function
    - using macros to access the arguments of the variadic function
    - using 2 function from myheader file

--- ft_printf.c
+++ ft_printf.c
@@ -1,32 +1,60 @@
 #include <stdarg.h>
+#include "myheader.h"
 
+// A variadic function is a function which accepts a variable
+// number of arguments. It is characterized by the "..." in a function.
+int ft_printf(const char *format, ...)
+{
 
-//A variadic function is a function which accepts a variable 
-//number of arguments. It is characterized by the "..." in a function.
-int	ft_printf(const char *format, ...){
+    // object type suitable for holding the information needed by
+    // the macros va_start, va_copy, va_arg, and va_end
+    va_list args;
 
-//object type suitable for holding the information needed by 
-//the macros va_start, va_copy, va_arg, and va_end
-    va_list    args;
-
-//The macro va_start function will somehow initialize everything, 
-//before we start moving through our variable argument list (va_list).
+    // The macro va_start function will somehow initialize everything,
+    // before we start moving through our variable argument list (va_list).
     va_start(args, format);
 
-//This macro allows access to the arguments of the variadic function. 
-   
-    va_arg(args, char *);
-//Each time va_arg is called, you move to the next argument. 
-    va_arg(args, int);
+    int n = 0;
+    int count = 0;
+    while (format[n] != '\0')
+    {
+        if (format[n] != '%')
+        {
+            ft_putchar(format[n]);
+            count++;
+        }
+        else
+        {
+            n++;
+            if (format[n] == 's')
+            {
+// Retrieve the next argument from the variable argument list (args)
+// We expect this argument to be a string (char *), so we cast it accordingly.
+// The value of 'str' will be the pointer to the string passed to the ft_printf function.
+                char *str = va_arg(args, char *);
+                int i = 0;
+// Print the string using array indexing to traverse each character
+                while (str[i])
+                {
+                    ft_putchar(str[i]);
+                    i++;
+                }
+                count += i;
+            }
+            else if (format[n] == 'd')
+            {
+                int num = va_arg(args, int);
+                ft_putnbr(num);
+                count++;
+            }
+        }
+        n++;
+    }
 
-
-
-
-
-
-
-//Once finished -> clean up the object initialized by calling va_start.
-//va_end can modify the object, which is called "args" 
-// so that it is no longer usable. 
+    // Once finished -> clean up the object initialized by calling va_start.
+    // va_end can modify the object, which is called "args"
+    //  so that it is no longer usable.
     va_end(args);
+
+    return count;
 }
