fa2543619a9f372142dd28d9e37c2ba3cbdaff2c
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-11-27 19:06:20 +0200

    Update myheader.c
    -putstr
    -putnbr_base

--- myheader.c
+++ myheader.c
@@ -1,5 +1,16 @@
 #include <unistd.h>
 
+void ft_putstr(char *s)
+{
+    int i = 0;
+
+    while (s[i])
+    {
+        write(1, &s[i], 1);
+        i++;
+    }
+}
+
 void ft_putchar(char c)
 {
     write(1, &c, 1);
@@ -30,11 +41,34 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void ft_putnbr_unsigned(n)
+void ft_putnbr_unsigned(unsigned int n)
 {
     if (n >= 10)
     {
         ft_putnbr_unsigned(n / 10);
     }
     ft_putchar(n % 10 + '0');
+}
+
+void ft_putnbr_base(int num, int base, int uppercase)
+{
+    char *digits;
+
+    // Choose the digit set based on uppercase/lowercase for hex (and others if needed)
+    if (uppercase)
+    {
+        digits = "0123456789ABCDEF";
+    }
+    else
+    {
+        digits = "0123456789abcdef";
+    }
+
+    if (num >= (unsigned long)base)
+    {
+        // Print the higher digits first
+        ft_putchar_base(num / base, base, uppercase);
+    }
+
+    ft_putchar(digits[num % base]); // Print the current digit
 }
