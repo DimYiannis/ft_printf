4a5b6123aad52a19f6264c2cdf25f79d9057d004
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-11-28 22:36:10 +0200

    Update myheader.h

--- myheader.h
+++ myheader.h
@@ -17,10 +17,22 @@ void ft_putchar(char c);
 void ft_putnbr_unsigned(unsigned int n);
 
 //function to print a hexadecimal
-void ft_putnbr_base(unsigned int num, int base, int uppercase );
+void ft_putnbr_base(int num, int base, int uppercase);
 
 //function to print a string
-void ft_putstr(char *s) 
+void ft_putstr(char *s); 
+
+// Helper function to calculate the length of an integer when printed
+int ft_intlen(int num);
+
+// Helper function to calculate the length of an unsigned integer when printed
+int ft_uintlen(unsigned int num);
+
+// Helper function to calculate the length of a string
+int ft_strlen(char *s);
+
+//Helper function to calculate the length of a number in a specific base
+int ft_base_len(unsigned long num, int base)
 
 
 #endif
